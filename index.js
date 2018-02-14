5b7ba8195756609a53052883a86d52c8d3bf3eb0
Phil Stavri <p.stavri@gmail.com>
2018-02-14 00:30:39 +0000

    Read nginx conf and parse comments, resolve condition and add into conf. Write back to conf and config.

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-let argv = require("yargs").argv;
+let argv = require("yargs")
+            .array("on")
+            .array("off")
+            .argv;
 let fs = require("fs");
 let path = require("path");
 
 const FILENAME = argv.filename || "nginx-if.json";
+const ON = argv.on;
+const OFF = argv.off; 
 
-let cwdPath = path.join(process.cwd(), FILENAME);
-let dirPath = path.join(__dirname, FILENAME);
-let config;
+function resolvePath(filename) {
+    let cwdPath = path.join(process.cwd(), filename);
+    let dirPath = path.join(__dirname, filename);
 
-if(fs.existsSync(cwdPath)){
-    console.log("found at cwd");
-    config = require(cwdPath);
-} else if (fs.existsSync(dirPath)){
-    console.log("found at dir");
-    config = require(dirPath);
+    if(fs.existsSync(cwdPath)){
+        return cwdPath;
+    } else if (fs.existsSync(dirPath)){
+        return dirPath;
+    }
+
+    throw new Error(`File ${filename} was not found locally or in cwd`);
 }
 
+const REGEX = /(#ngx\-if start\s*)([\s\S]*?)(\s*#ngx\-if end)/gm;
+
+
+let configPath = resolvePath(FILENAME);
+let config = require(configPath);
+let nginxConfFilename = config.options.nginxConfFilename || "nginx.conf";
+let nginxConfPath = resolvePath(nginxConfFilename);
+
 console.log("config", config);
+console.log("nginxConfPath", nginxConfPath);
+
+let nginxConf = fs.readFileSync(nginxConfPath, "utf8");
+console.log("nginxConf", nginxConf);
+console.log("ON", ON);
+console.log("OFF", OFF);
+
+if(REGEX.test(nginxConf)){
+
+    let statements = config.conditions.map(function(condition) {
+
+        if(ON && ON.includes(condition.name)) {
+            condition.isOn = true;
+        } else if(OFF && OFF.includes(condition.name)){
+            condition.isOn = false;
+        }
+
+        return condition.isOn ? condition.on : condition.off;
+    }).join("\n");
+
+    console.log("statements", statements);
+
+    nginxConf = nginxConf.replace(REGEX, function(match, openComment, previousStatements, closeComment){
+        return openComment + statements +closeComment;
+    });
+
+    fs.writeFileSync(nginxConfPath, nginxConf, 'utf8');
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 2), 'utf8');
+}
 
 
